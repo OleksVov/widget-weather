9fd17ec5e5bd96778565ed3f5bb7b65d766fe0f9
Oleksandr <alex.vovchok@gmail.com>
2023-03-24 00:36:19 +0200

    added location

--- src/index.js
+++ src/index.js
@@ -1,64 +1,64 @@
 import moment from "moment/moment";
 
 const weatherBlock = document.querySelector('#weather');
-const locationBtn = document.querySelector('.weather_city');
-const weatherTemp = document.querySelector('.weather_temp');
-const weatherStatus = document.querySelector('.weather_status');
-const weatherIcon = document.querySelector('.weather_icon');
-const currentDate = document.querySelector('.weather_date');
 
-const apiKey = '92b7ae078a5ceba812c34c84b6f882cb';
+const apiWeather = '92b7ae078a5ceba812c34c84b6f882cb';
+let server;
 
-let latitude  = 40.71;
-let longitude = -74.00;
+const date = moment(new Date());
+const resultMonth = date.format("D MMM YYYY");
+const resultDay = date.format('ddd')
 
-// locationBtn.addEventListener('click', () => {
-//     if(navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(onSuccess);
-//     } else {
-//         alert("Your browser not support geolocation api");
-//     }
-// })
-// function onSuccess(position) {
-//     let latitude  = position.coords.latitude;
-//     let longitude = position.coords.longitude;
-    
-  
-// }
+
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(onSuccess);
+} else {
+    alert("Your browser not support geolocation api");
+}
+
+function onSuccess(position) {
+let latitude  = position.coords.latitude;
+let longitude = position.coords.longitude;
+server = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude }&lon=${longitude}&units=metric&appid=${apiWeather}`;
+loadWeather();
+
+}
 
 
 
 async function loadWeather () {
-    const server = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude }&lon=${longitude}&units=metric&appid=${apiKey}`;
-    const response = await fetch(server);
-    const responseResult = await response.json();
-    
-    if (response.ok) {
-        getWeather(responseResult);
-    } else {
-        weatherBlock.innerHTML = responseResult.message;
-    }
+    try {
+        const response = await fetch(server);
+        const responseResult = await response.json();
+        renderWeather(responseResult);
+    } catch (error) {
+        console.log(error.message);
     }
 
-function getWeather(data) {
-console.log(data);
+function renderWeather(data) {
+
 const location = data.name;
 const temp = Math.round(data.main.temp);
 const status = data.weather[0].main;
 const icon = data.weather[0].icon;
 
-locationBtn.textContent = `${location}`;
-weatherTemp.textContent = `${temp}`;
-weatherStatus.textContent = `${status}`;
-weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${icon}@2x.png`);
-}
 
-if (weatherBlock){
-    loadWeather ()
-}
+const markup = `<div class="weather-header">
+<p class="weather-temp">${temp}&deg;</p>
+<div class="weather-main">
+  <p class="weather-status">${status}</p>
+  <div class="weather-city">
+  <img class="location-icon" src ="../images/carbon_location.svg" alt = "">
+    ${location}
+    </div>
+</div>
+</div>
 
-// const date = new Date().toDateString();
-// currentDate.textContent = `${date}`;
-const date = moment(new Date());
-const resultDate = date.format("D MMM YYYY");
-currentDate.textContent = `${resultDate}`;
+<img class="weather-icon" src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="${status}" >
+
+<ul class="weather-date">
+<li class="weather-day">${resultDay}</li>
+<li class="weather-month">${resultMonth}</li>
+</ul>`
+weatherBlock.innerHTML = markup;
+}
